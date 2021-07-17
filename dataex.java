8901d086545d66b978ccccfe9c80f7a529d6a0e7
Balakumaran <bkumaran121@gmail.com>
2021-07-17 14:15:24 +0530

    Second

---
+++ dataex.java
@@ -0,0 +1,12 @@
+class dataex
+{
+	public static void main(String ar[])
+	{
+		byte a=65;
+		byte b='e';
+		System.out.println("A is:"+a);
+		System.out.println("B is:"+b);
+		System.out.println("A is:"+a+" "+"B is:"+b);
+	}
+}
+		
